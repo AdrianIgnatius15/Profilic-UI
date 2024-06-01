23cafb1502e1bc6d8eb7b4a185ecba1df5b48576
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-01 23:18:47 +0800

    Added conditional pages to load

--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -1,6 +1,17 @@
 import { observer } from "mobx-react-lite";
 import Header from "../header/Header";
+import { useStore } from "../../stores/contextStore/store.context";
+import Landing from "../../pages/Landing";
+import Home from "../../pages/Home";
 
 export default observer(function Index(): JSX.Element {
-   return <Header />;
+    const { accountStore } = useStore();
+    const { user } = accountStore;
+    return (
+        <>
+            <Header />
+            {user === null && <Landing />}
+            {user !== null && <Home />}
+        </>
+    );
 });
