e4965b2ddd7f259c1910837f1b1ad6a2babaf8cf
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-01 23:19:21 +0800

    Added initial logic for Landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,23 @@
 import { observer } from "mobx-react-lite";
+import { useStore } from "../stores/contextStore/store.context";
+import { useEffect } from "react";
+import Carousel from "react-material-ui-carousel";
 
 export default observer(function Landing(): JSX.Element {
-   return <></>;
+    const { photoStore } = useStore();
+    const { bannerPhotos, retrieveBannerPhotos } = photoStore;
+
+    useEffect(() => {
+        if (bannerPhotos.length === 0) {
+            retrieveBannerPhotos();
+        }
+    }, [photoStore, bannerPhotos.length]);
+
+    return (
+        <Carousel animation="slide" autoPlay sx={{ position: "relative" }}>
+            {bannerPhotos.map((item) => (
+                <img src={item.photoURL} alt={item?.name} key={item.id} width={"max-content"} />
+            ))}
+        </Carousel>
+    );
 });
