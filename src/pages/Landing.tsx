05b545ecccd57ed08193e067225c955e658bddfb
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-02 19:21:38 +0800

    Added new styling into the Landing page for Carousel

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,6 +3,7 @@ import { useStore } from "../stores/contextStore/store.context";
 import { useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
+import "./pages-styles/landing.css";
 
 export default observer(function Landing(): JSX.Element {
     const { photoStore } = useStore();
@@ -15,10 +16,15 @@ export default observer(function Landing(): JSX.Element {
     }, [photoStore, bannerPhotos.length]);
 
     return (
-        <Carousel animation="slide" autoPlay sx={{ width: "85%", position: "absolute", marginTop: "1rem" }}>
+        <Carousel animation="slide" autoPlay className="carousel">
             {bannerPhotos.map((item) => (
                 <Paper key={item.id}>
-                    <img src={item.photoURL} alt={item?.name} key={item.id} style={{ width: "100%", height: "100vh", minHeight: "60rem" }} />
+                    <img src={item.photoURL} alt={item?.name} key={item.id} className="image-carousel" />
+
+                    <div>
+                        <h2>All my hot photos, right here!</h2>
+                        <p>My photoshoots from the past, present and future in my all new site</p>
+                    </div>
                 </Paper>
             ))}
         </Carousel>
