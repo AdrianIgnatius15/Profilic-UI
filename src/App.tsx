683371f2f600b4eb2e938c1a00b43a5affc8d150
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-04 22:25:31 +0800

    No changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,24 @@ import "./App.css";
 import Index from "./components/index/Index";
 
 function App() {
-   const theme = createTheme({
-      palette: {
-         primary: {
-            main: colors.pink[300],
-         },
-         secondary: {
-            main: colors.blue[400],
-         },
-      },
-   });
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: colors.pink[300],
+            },
+            secondary: {
+                main: colors.blue[400],
+            },
+        },
+    });
 
-   return (
-      <ThemeProvider theme={theme}>
-         <div>
-            <Index />
-         </div>
-      </ThemeProvider>
-   );
+    return (
+        <ThemeProvider theme={theme}>
+            <div>
+                <Index />
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
