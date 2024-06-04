85f799296bb3cf4a41430b321086b3cb3b4d6cda
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-04 22:25:50 +0800

    Added improved styling to carousel

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,9 @@
 import { observer } from "mobx-react-lite";
 import { useStore } from "../stores/contextStore/store.context";
 import { useEffect } from "react";
-import Carousel from "react-material-ui-carousel";
-import { Paper } from "@mui/material";
 import "./pages-styles/landing.css";
+import { Box, Typography } from "@mui/material";
+import Carousel from "react-material-ui-carousel";
 
 export default observer(function Landing(): JSX.Element {
     const { photoStore } = useStore();
@@ -18,14 +18,18 @@ export default observer(function Landing(): JSX.Element {
     return (
         <Carousel animation="slide" autoPlay className="carousel">
             {bannerPhotos.map((item) => (
-                <Paper key={item.id}>
-                    <img src={item.photoURL} alt={item?.name} key={item.id} className="image-carousel" />
-
+                <Box component={"div"} key={item.id}>
                     <div>
-                        <h2>All my hot photos, right here!</h2>
-                        <p>My photoshoots from the past, present and future in my all new site</p>
+                        <img src={item.photoURL} alt={item.photoURL} srcSet={item.photoURL} className="image-carousel" />
+                        <Box component={"div"} className="text-carousel">
+                            <Typography gutterBottom variant="h5" component="div">
+                                All my hot photos, right here!
+                            </Typography>
+
+                            <Typography variant="body2">My photoshoots from the past, present and future in my all new site</Typography>
+                        </Box>
                     </div>
-                </Paper>
+                </Box>
             ))}
         </Carousel>
     );
