5632b1e2567b6d17842428d0efa30bc2610bfe4e
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-06 21:27:13 +0800

    Added logo into IconButton

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,6 @@
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../stores/contextStore/store.context";
-import {
-   AppBar,
-   Box,
-   Container,
-   Drawer,
-   IconButton,
-   List,
-   ListItemButton,
-   ListItemText,
-   Toolbar,
-   Typography,
-} from "@mui/material";
+import { AppBar, Box, Container, Drawer, IconButton, List, ListItemButton, ListItemText, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import LoginIcon from "@mui/icons-material/Login";
 import InfoIcon from "@mui/icons-material/Info";
@@ -21,89 +10,85 @@ import logo from "../../assets/pv-logo.png";
 import { useState } from "react";
 
 export default observer(function NavBar(): JSX.Element {
-   const { accountStore } = useStore();
-   const { user } = accountStore;
+    const { accountStore } = useStore();
+    const { user } = accountStore;
 
-   const [openCloseFlag, setOpenCloseFlag] = useState<boolean>(false);
+    const [openCloseFlag, setOpenCloseFlag] = useState<boolean>(false);
 
-   const openCloseDrawer = () => {
-      setOpenCloseFlag(!openCloseFlag);
-   };
+    const openCloseDrawer = () => {
+        setOpenCloseFlag(!openCloseFlag);
+    };
 
-   return (
-      <div className="header">
-         <AppBar variant="elevation" position="static">
+    return (
+        <AppBar position="static">
             <Container maxWidth="xl">
-               <Toolbar disableGutters>
-                  <IconButton
-                     size="large"
-                     edge="start"
-                     color="secondary"
-                     aria-label="menu"
-                     sx={{ mr: 2 }}
-                     disableFocusRipple
-                     disableRipple
-                     onClick={openCloseDrawer}
-                  >
-                     <MenuIcon />
-                  </IconButton>
-
-                  {user && (
-                     <IconButton
+                <Toolbar disableGutters variant="regular">
+                    <IconButton
                         size="large"
-                        aria-label="account of the current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="false"
-                        color="inherit"
-                     ></IconButton>
-                  )}
+                        edge="start"
+                        color="secondary"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        disableFocusRipple
+                        disableRipple
+                        onClick={openCloseDrawer}
+                    >
+                        <MenuIcon />
+                    </IconButton>
 
-                  <img
-                     src={logo}
-                     alt="Petra Central Logo"
-                     style={{
-                        width: "15rem",
-                        maxWidth: "15rem",
-                        minWidth: "10rem",
-                     }}
-                  />
+                    {user && (
+                        <IconButton
+                            size="large"
+                            aria-label="account of the current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="false"
+                            color="inherit"
+                        ></IconButton>
+                    )}
 
-                  <Drawer
-                     anchor="left"
-                     open={openCloseFlag}
-                     onClose={openCloseDrawer}
-                  >
-                     <Box p={2} width={"250px"} textAlign={"center"}>
-                        <Typography variant="h6" component={"div"}>
-                           Menu
-                        </Typography>
+                    <IconButton size="large" edge="start" color="secondary" aria-label="menu" sx={{ mr: 2 }} disableFocusRipple disableRipple>
+                        <img
+                            src={logo}
+                            alt="Petra Central Logo"
+                            style={{
+                                width: "100%",
+                                maxWidth: "15rem",
+                                minWidth: "16px",
+                            }}
+                        />
+                    </IconButton>
 
-                        <List>
-                           <ListItemButton>
-                              <HomeIcon sx={{ mr: 2 }} />
-                              <ListItemText primary="Home" />
-                           </ListItemButton>
+                    <Drawer anchor="left" open={openCloseFlag} onClose={openCloseDrawer} color="secondary">
+                        <Box p={2} width={"250px"} textAlign={"center"}>
+                            <Typography variant="h6" component={"div"}>
+                                Menu
+                            </Typography>
 
-                           <ListItemButton>
-                              <InfoIcon sx={{ mr: 2 }} />
-                              <ListItemText primary="About" />
-                           </ListItemButton>
+                            <List>
+                                <ListItemButton>
+                                    <HomeIcon sx={{ mr: 2 }} />
+                                    <ListItemText primary="Home" />
+                                </ListItemButton>
 
-                           <ListItemButton>
-                              <LoginIcon sx={{ mr: 2 }} />
-                              <ListItemText primary="Login" />
-                           </ListItemButton>
+                                <ListItemButton>
+                                    <InfoIcon sx={{ mr: 2 }} />
+                                    <ListItemText primary="About" />
+                                </ListItemButton>
 
-                           <ListItemButton>
-                              <HowToRegIcon sx={{ mr: 2 }} />
-                              <ListItemText primary="Register" />
-                           </ListItemButton>
-                        </List>
-                     </Box>
-                  </Drawer>
-               </Toolbar>
+                                <ListItemButton>
+                                    <LoginIcon sx={{ mr: 2 }} />
+                                    <ListItemText primary="Login" />
+                                </ListItemButton>
+
+                                <ListItemButton>
+                                    <HowToRegIcon sx={{ mr: 2 }} />
+                                    <ListItemText primary="Register" />
+                                </ListItemButton>
+                            </List>
+                        </Box>
+                    </Drawer>
+                </Toolbar>
             </Container>
-         </AppBar>
-      </div>
-   );
+        </AppBar>
+    );
 });
