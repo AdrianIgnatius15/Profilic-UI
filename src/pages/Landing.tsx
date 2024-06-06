c360507424386bb08f8e6f0b8e00da2550b1bf7d
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-06 19:44:41 +0800

    Removed code and transferred to another reusable component

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,8 @@
 import { observer } from "mobx-react-lite";
 import { useStore } from "../stores/contextStore/store.context";
 import { useEffect } from "react";
-import "./pages-styles/landing.css";
-import { Box, Typography } from "@mui/material";
-import Carousel from "react-material-ui-carousel";
+import { Container } from "@mui/material";
+import CarouselComponent from "../components/carousel/CarouselComponent";
 
 export default observer(function Landing(): JSX.Element {
     const { photoStore } = useStore();
@@ -16,21 +15,18 @@ export default observer(function Landing(): JSX.Element {
     }, [photoStore, bannerPhotos.length]);
 
     return (
-        <Carousel animation="slide" autoPlay className="carousel">
-            {bannerPhotos.map((item) => (
-                <Box component={"div"} key={item.id}>
-                    <div>
-                        <img src={item.photoURL} alt={item.photoURL} srcSet={item.photoURL} className="image-carousel" />
-                        <Box component={"div"} className="text-carousel">
-                            <Typography gutterBottom variant="h5" component="div">
-                                All my hot photos, right here!
-                            </Typography>
-
-                            <Typography variant="body2">My photoshoots from the past, present and future in my all new site</Typography>
-                        </Box>
-                    </div>
-                </Box>
-            ))}
-        </Carousel>
+        <Container
+            maxWidth="xl"
+            component="div"
+            sx={{
+                display: "flex",
+                height: "700px",
+                width: "100%",
+                maxWidth: "800px",
+                my: 2,
+            }}
+        >
+            <CarouselComponent photos={bannerPhotos} />
+        </Container>
     );
 });
