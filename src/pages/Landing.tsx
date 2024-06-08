0578e2f53b46a0c6a21b1295383e6bcc12d5a339
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-09 00:15:44 +0800

    Added cards and made responsive

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,13 @@
 import { observer } from "mobx-react-lite";
 import { useStore } from "../stores/contextStore/store.context";
 import { useEffect } from "react";
-import { Container } from "@mui/material";
+import { Card, CardContent, Container, Typography } from "@mui/material";
 import CarouselComponent from "../components/carousel/CarouselComponent";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 export default observer(function Landing(): JSX.Element {
     const { photoStore, carouselCaptionStore } = useStore();
-    const { bannerPhotos, retrieveBannerPhotos } = photoStore;
+    const { bannerPhotos, numberOfPhotosAvailable, retrieveBannerPhotos, retrieveNumberOfPhotosAvailable } = photoStore;
     const { carouselCaptions, getCarouselCaptions } = carouselCaptionStore;
 
     useEffect(() => {
@@ -21,6 +21,12 @@ export default observer(function Landing(): JSX.Element {
             getCarouselCaptions();
         }
     }, [carouselCaptionStore, carouselCaptions.length]);
+
+    useEffect(() => {
+        if (numberOfPhotosAvailable === 0) {
+            retrieveNumberOfPhotosAvailable();
+        }
+    }, [photoStore, numberOfPhotosAvailable !== 0]);
 
     return (
         <Grid2 container width={"100%"}>
@@ -38,6 +44,40 @@ export default observer(function Landing(): JSX.Element {
                 >
                     <CarouselComponent photos={bannerPhotos} carouselCaptions={carouselCaptions} />
                 </Container>
+            </Grid2>
+
+            <Grid2 spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                <Grid2 xs={2} sm={4} md={4}>
+                    <Card sx={{ maxWidth: 275 }} variant="elevation">
+                        <CardContent>
+                            <Typography variant="h4" component="div">
+                                {numberOfPhotosAvailable}
+                                photos
+                            </Typography>
+                            <Typography>photos made available</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card sx={{ maxWidth: 275 }} variant="elevation">
+                        <CardContent>
+                            <Typography variant="h4" component="div">
+                                {numberOfPhotosAvailable}
+                                videos
+                            </Typography>
+                            <Typography>videos made available</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card sx={{ maxWidth: 275 }} variant="elevation">
+                        <CardContent>
+                            <Typography variant="h4" component="div">
+                                {numberOfPhotosAvailable}
+                                albums
+                            </Typography>
+                            <Typography>albums made available</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid2>
             </Grid2>
         </Grid2>
     );
