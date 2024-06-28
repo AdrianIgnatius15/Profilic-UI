b6603930c3a9a84cea34ae76d5a72bd265fef8b1
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-06-28 23:08:07 +0800

    Added custom fonts to be loaded into the theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider, colors, createTheme } from "@mui/material";
 import "./App.css";
+import "../public/fonts/fonts.css";
 import Index from "./components/index/Index";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -14,6 +15,14 @@ function App() {
                 main: colors.pink[50],
             },
             divider: colors.brown["A100"],
+        },
+        typography: {
+            fontFamily: [
+                "Noto Serif Display Condensed Regular",
+                "Noto Serif Display Condensed Medium",
+                "Noto Serif Display Condensed Italic",
+                "Noto Serif Display Condensed Bold",
+            ].join(","),
         },
         components: {
             MuiDrawer: {
