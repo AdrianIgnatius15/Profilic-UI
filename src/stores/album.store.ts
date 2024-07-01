6d4179d898db220d111cadcfde621dd8e609a350
adrianignatius15@hotmail.com <adrianignatius15@hotmail.com>
2024-07-02 02:10:59 +0800

    Added store tracker to track data from endpoint on retrieving the image lists

--- src/stores/album.store.ts
+++ src/stores/album.store.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { makeAutoObservable, runInAction } from "mobx";
+import { PhotoReadDto } from "../models/photo-read-dto.model";
 
 export default class AlbumStore {
     numberOfAlbumsAvailable: number = 0;
+    albumImageLists: PhotoReadDto[] = [];
 
     constructor() {
         makeAutoObservable(this);
@@ -24,6 +26,21 @@ export default class AlbumStore {
         } catch (error) {
             const errorCaptured = error as Error;
             console.info("Error captured during retrieving number of albums due to", errorCaptured.message);
+        }
+    };
+
+    getPhotosForImageList = async () => {
+        try {
+            const imageList = await axios.get<PhotoReadDto[]>(`http://localhost:8080/v1/dev/api/album/list/image-list`);
+
+            if (imageList && imageList?.status === 200 && imageList?.data) {
+                runInAction(() => {
+                    this.albumImageLists = imageList.data;
+                });
+            }
+        } catch (error) {
+            const errorCaptured = error as Error;
+            console.info("Error captured during retrieving image list due to", errorCaptured.message);
         }
     };
 }
